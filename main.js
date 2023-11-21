87ecb23897ab36f28f2a167f42081e4048e5b123
Disney <disneyconstant996@gmail.com>
2023-11-21 11:29:27 +0300

    Added the rate again button

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const initialState = document.querySelector(".selection-slate");
 const finalState = document.querySelector(".thankYouSlate");
 
 const ratingDigits = document.querySelectorAll(".rating-digit");
-const btn = document.getElementById("btn");
+const submit = document.getElementById("submit");
 const para = document.querySelector(".choice-preview")
+const rateAgain = document.getElementById("rate_again");
 
 
 ratingDigits.forEach(element => {
@@ -14,8 +15,13 @@ ratingDigits.forEach(element => {
 })
 
 
-btn.addEventListener("click", () => {
+submit.addEventListener("click", () => {
     initialState.style.display = "none";
     finalState.style.display = "block";
 
+})
+
+rateAgain.addEventListener("click", () => {
+    initialState.style.display = "block";
+    finalState.style.display = "none";
 })
